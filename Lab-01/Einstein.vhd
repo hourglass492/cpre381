53f181f79b2e0e938ec9bbac314627328f6100ce
Nicholas <nk3@iastate.edu>
2020-08-22 14:01:57 -0500

    first commit with lab 1

---
+++ Lab-01/Einstein.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity Multiplier is
+
+  port(iCLK             : in std_logic;
+       iA               : in integer;
+       iB               : in integer;
+       oC               : out integer);
+
+end Multiplier;
+
+architecture behavior of Multiplier is
+begin
+
+  process(iCLK, iA, iB)
+  begin
+    if rising_edge(iCLK) then
+	   oC <= iA * iB;
+    end if;
+  end process;
+  
+end behavior;
