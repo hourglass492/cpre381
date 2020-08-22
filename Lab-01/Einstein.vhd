c01718f766b12f1f019ec9aa1f122118c4827c77
hourglass <nk3@iastate.edu>
2020-08-22 14:23:12 -0500

    hopefully done with einstien for lab 1

--- Lab-01/Einstein.vhd
+++ Lab-01/Einstein.vhd
@@ -1,23 +1,43 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity Multiplier is
+
+entity Einstein is
 
   port(iCLK             : in std_logic;
-       iA               : in integer;
-       iB               : in integer;
-       oC               : out integer);
+       mass 		        : in integer;
+       energy 		      : out integer);
 
-end Multiplier;
+end Einstein;
 
-architecture behavior of Multiplier is
+architecture structure of Einstein is
+  
+  -- Describe the component entities as defined in Adder.vhd 
+  -- and Multiplier.vhd (not strictly necessary).
+
+  component Multiplier
+    port(iCLK           : in std_logic;
+         iA             : in integer;
+         iB             : in integer;
+         oC             : out integer);
+  end component;
+
+  -- Constant of C squared for E= Mc^2, using c = 9487 so c squared is 90003169
+  constant C2 : integer := 90003169;
+
+
 begin
 
-  process(iCLK, iA, iB)
-  begin
-    if rising_edge(iCLK) then
-	   oC <= iA * iB;
-    end if;
-  end process;
   
-end behavior;
+  ---------------------------------------------------------------------------
+  -- Level 1: Calculate C2 * mass
+  ---------------------------------------------------------------------------
+  g_Mult1: Multiplier
+    port MAP(iCLK             => iCLK,
+             iA               => C2,
+             iB               => mass,
+             oC               => energy);
+
+     
+  
+end structure;
