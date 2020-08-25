f8da98fe83343fe075d913e6b53743d6954af574
hourglass <nk3@iastate.edu>
2020-08-25 14:55:43 -0500

    finish lab 2 prelab

---
+++ Lab-02/prelab.vhd
@@ -0,0 +1,23 @@
+
+
+
+
+-- problem 4 b
+
+
+
+entity sys2 is
+    port (
+        input_w     : in std_logic;
+        a_data      : in std_logic_vector(0 to 7);
+        b_data      : in std_logic_vector(0 to 7);
+        clik        : in std_logic;
+        dat_4       : out std_logic_vector(0 to 7);
+        dat_4       : out std_logic_vector(0 to 3);
+    );
+end sys2;
+
+
+
+
+
