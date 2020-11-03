cc56738f9699b9b29d6387d50444e03e0bab7214
Nicholas <nk3@iastate.edu>
2020-11-02 18:35:34 -0600

    before Ichange where my pc starts

--- cpre381-toolflow-release/MARsWork/Examples/simplebranch.asm
+++ cpre381-toolflow-release/MARsWork/Examples/simplebranch.asm
@@ -1,6 +1,8 @@
 main:
-	#j exit
-	#ori $s0, $zero 0x1234
+	ori $s0, $zero 0x1234
+	
+	
+
 	#j skip
 	#li $s0 0xffffffff
 #skip:
@@ -8,17 +10,33 @@ main:
 #	beq $s0 $s1 skip2
 	#li $s0 0xffffffff
 #skip2:
+
+	ori $s3 $zero 0xBBBB
+	ori $s3 $zero 0xBBBB
+	ori $s3 $zero 0xBBBB
+	ori $s3 $zero 0xBBBB
 	jal fun
-	#ori $s3 $zero 0x1234
 	
 	#bne $zero, $s0 exit
-	#ori $s4 $zero 0x1234
-	j exit
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
+	ori $s4 $zero 0xFFFF
 
-exit:
-	li   $v0, 10          # system call for exit
-      	syscall               # Exit!
-		
+	j exit
 fun:
 	ori $s2 $zero 0x1234
+	ori $s2 $zero 0x1234
+	ori $s2 $zero 0x1234
+	ori $s2 $zero 0x1234
+	ori $s2 $zero 0x1234
 	jr $ra
+exit:
+	li   $v0, 10          # system call for exit
+	ori $s0, $s0, 0
+      	syscall               # Exit!
+		
